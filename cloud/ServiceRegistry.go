0cc7dcc7f146f5601d9c98cb9f592ce3c4171d89
liulei <193002818@qq.com>
2023-07-19 20:43:36 +0800

    'add'

--- cloud/ServiceRegistry.go
+++ cloud/ServiceRegistry.go
@@ -1,10 +1,14 @@
 package cloud
 
+import "io"
+
 type ServiceRegistry interface {
 	Register(serviceInstance ServiceInstance) bool
 
+	Deregister()
+
 	FindServices(serviceName string) []ServiceInstance
 
-	Deregister()
+	RequestApiByService(serviceName string, method string, path string, body io.Reader) (string, error)
 }
 

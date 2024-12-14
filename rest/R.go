45922e9c0d1900cf20e28328cbfac6965a68b562
valuetodays <abcdefg20111111@gmail.com>
2024-12-14 15:52:02 +0800

    feat: add OfError() OfSuccess()

--- rest/R.go
+++ rest/R.go
@@ -7,3 +7,17 @@ type R struct {
 	Msg  string `json:"msg"`
 	Data any    `json:"data"`
 }
+
+func OfError(code int32, msg string) *R {
+	return &R{
+		Msg:  msg,
+		Code: code,
+	}
+}
+
+func OfSuccess(data any) *R {
+	return &R{
+		Code: CodeForSuccess,
+		Data: data,
+	}
+}
